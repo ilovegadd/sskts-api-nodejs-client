e211269538928dac5156a5bd2fc3cd070c7b2546
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2017-08-21 09:25:26 +0000

    # SSKTS-561 - remove person services that require member login

--- lib/service/person.ts
+++ lib/service/person.ts
@@ -4,9 +4,9 @@
  * @namespace service.person
  */
 
-import * as sskts from '@motionpicture/sskts-domain';
-import { CREATED, NO_CONTENT, OK } from 'http-status';
-import apiRequest from '../apiRequest';
+// import * as sskts from '@motionpicture/sskts-domain';
+// import { CREATED, NO_CONTENT, OK } from 'http-status';
+// import apiRequest from '../apiRequest';
 
 import { Service } from '../service';
 
@@ -21,89 +21,4 @@ export interface IPresavedCreditCardTokenized {
 }
 
 export class PersonService extends Service {
-    /**
-     * プロフィール取得
-     */
-    public async getProfile(params: {
-        /**
-         * 人物ID
-         * ログイン中の人物の場合、'me'を指定してください。
-         */
-        personId: string;
-    }): Promise<sskts.factory.person.IProfile> {
-        return await apiRequest({
-            auth: this.options.auth,
-            baseUrl: this.options.endpoint,
-            uri: `/people/${params.personId}/profile`,
-            method: 'GET',
-            expectedStatusCodes: [OK]
-        });
-    }
-
-    /**
-     * プロフィール変更
-     */
-    public async updateProfile(params: {
-        /**
-         * 人物ID
-         * ログイン中の人物の場合、'me'を指定してください。
-         */
-        personId: string;
-        /**
-         * プロフィール
-         */
-        profile: sskts.factory.person.IProfile
-    }): Promise<void> {
-        return await apiRequest({
-            auth: this.options.auth,
-            baseUrl: this.options.endpoint,
-            uri: `/people/${params.personId}/profile`,
-            body: params.profile,
-            method: 'PUT',
-            expectedStatusCodes: [NO_CONTENT]
-        });
-    }
-
-    /**
-     * クレジットカード検索
-     */
-    public async findCreditCards(params: {
-        /**
-         * 人物ID
-         * ログイン中の人物の場合、'me'を指定してください。
-         */
-        personId: string;
-    }): Promise<sskts.GMO.services.card.ISearchCardResult[]> {
-        return await apiRequest({
-            auth: this.options.auth,
-            baseUrl: this.options.endpoint,
-            uri: `/people/${params.personId}/creditCards`,
-            method: 'GET',
-            expectedStatusCodes: [OK]
-        });
-    }
-
-    /**
-     * クレジットカード追加
-     */
-    public async addCreditCard(params: {
-        /**
-         * 人物ID
-         * ログイン中の人物の場合、'me'を指定してください。
-         */
-        personId: string;
-        /**
-         * クレジットカード情報
-         */
-        creditCard: IPresavedCreditCardRaw | IPresavedCreditCardTokenized
-    }): Promise<sskts.GMO.services.card.ISearchCardResult> {
-        return await apiRequest({
-            auth: this.options.auth,
-            baseUrl: this.options.endpoint,
-            uri: `/people/${params.personId}/creditCards`,
-            body: params.creditCard,
-            method: 'POST',
-            expectedStatusCodes: [CREATED]
-        });
-    }
 }
--- types/service/person.d.ts
+++ types/service/person.d.ts
@@ -3,7 +3,6 @@
  *
  * @namespace service.person
  */
-import * as sskts from '@motionpicture/sskts-domain';
 import { Service } from '../service';
 export interface IPresavedCreditCardRaw {
     cardNo: string;
@@ -15,52 +14,4 @@ export interface IPresavedCreditCardTokenized {
     token: string;
 }
 export declare class PersonService extends Service {
-    /**
-     * プロフィール取得
-     */
-    getProfile(params: {
-        /**
-         * 人物ID
-         * ログイン中の人物の場合、'me'を指定してください。
-         */
-        personId: string;
-    }): Promise<sskts.factory.person.IProfile>;
-    /**
-     * プロフィール変更
-     */
-    updateProfile(params: {
-        /**
-         * 人物ID
-         * ログイン中の人物の場合、'me'を指定してください。
-         */
-        personId: string;
-        /**
-         * プロフィール
-         */
-        profile: sskts.factory.person.IProfile;
-    }): Promise<void>;
-    /**
-     * クレジットカード検索
-     */
-    findCreditCards(params: {
-        /**
-         * 人物ID
-         * ログイン中の人物の場合、'me'を指定してください。
-         */
-        personId: string;
-    }): Promise<sskts.GMO.services.card.ISearchCardResult[]>;
-    /**
-     * クレジットカード追加
-     */
-    addCreditCard(params: {
-        /**
-         * 人物ID
-         * ログイン中の人物の場合、'me'を指定してください。
-         */
-        personId: string;
-        /**
-         * クレジットカード情報
-         */
-        creditCard: IPresavedCreditCardRaw | IPresavedCreditCardTokenized;
-    }): Promise<sskts.GMO.services.card.ISearchCardResult>;
 }
