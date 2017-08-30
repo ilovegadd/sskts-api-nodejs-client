26caba4360f22ecef37ada040daf6caa400054e0
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2017-08-30 00:23:01 +0000

    # SSKTS-561 - strengthen order customer interface.

--- example/transaction/keepProcessingPlaceOrder.js
+++ example/transaction/keepProcessingPlaceOrder.js
@@ -4,17 +4,26 @@
  * @ignore
  */
 
+const readline = require('readline');
+
 const sasaki = require('../../lib/index');
 const processPlaceOrder = require('./processPlaceOrder');
 
-setInterval(
-    async () => {
-        try {
-            await processPlaceOrder.main();
-            console.log('order created');
-        } catch (error) {
-            console.error(error);
-        }
-    },
-    1000
-);
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('set intervals in milliseconds (example: 1000):\n', async (intervals) => {
+    setInterval(
+        async () => {
+            try {
+                await processPlaceOrder.main();
+                console.log('order created');
+            } catch (error) {
+                console.error(error);
+            }
+        },
+        intervals
+    );
+});
