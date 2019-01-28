6289366216eaa02982f6915b7ccf3507c19b076b
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2019-01-28 03:07:08 +0000

    # SSKTS-1133 ### Changed - 決済方法承認アクションインターフェースを強化

---
+++ example/transaction/processReturnOrder.js
@@ -0,0 +1,41 @@
+/**
+ * 注文返品プロセス
+ */
+const moment = require('moment');
+const auth = require('../authAsAdmin');
+const client = require('../../lib/index');
+
+async function main() {
+    // 管理者として操作する場合はこちら
+    const authClient = await auth.login();
+    await authClient.refreshAccessToken();
+    const loginTicket = authClient.verifyIdToken({});
+    console.log('username is', loginTicket.getUsername());
+
+    // const authClient = new client.auth.ClientCredentials({
+    //     domain: process.env.TEST_AUTHORIZE_SERVER_DOMAIN,
+    //     clientId: process.env.TEST_CLIENT_ID,
+    //     clientSecret: process.env.TEST_CLIENT_SECRET,
+    //     scopes: [],
+    //     state: ''
+    // });
+
+    const returnOrderService = new client.service.transaction.ReturnOrder({
+        endpoint: process.env.API_ENDPOINT,
+        auth: authClient
+    });
+
+    const transaction = await returnOrderService.start({
+        expires: moment().add(30, 'minutes').toDate(),
+        transactionId: '5c4ac06e2325362aa40cc09e',
+    });
+    console.log('transaction started', transaction.id);
+
+    console.log('confirming transaction...');
+    await returnOrderService.confirm({ transactionId: transaction.id });
+    console.log('transaction confirmed');
+}
+
+main().then(() => {
+    console.log('success!');
+}).catch(console.error);
